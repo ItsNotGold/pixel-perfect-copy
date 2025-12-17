be0bf1545758d8062f827338ab390e5e59b88a77
crm-maker <christian.malpaga@lyceestendhal.it>
2025-12-17 14:26:30 +0000

    16.1

--- src/data/multilingualContent/index.ts
+++ src/data/multilingualContent/index.ts
@@ -1,1 +1,7 @@
 export * from "@/data/exercisesContent";
+export * from "./precisionSwap";
+export * from "./reverseDefinitions";
+export * from "./synonymChain";
+export * from "./speakingTopics";
+export * from "./contextSwitch";
+export * from "./wordIncorporation";
